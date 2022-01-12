443d62edbbae62e2fca28d544afdfaeb1e94c38c
ghufrandali <ghufrandali1999@gmail.com>
2022-01-12 16:33:19 +0200

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.first.app',
+  appName: 'first-mobile-project',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
